7c73606471d471627453b73c6f7ca07eddd83311
ankanbag101 <bagankan@gmail.com>
2022-05-24 21:18:57 +0530

    update: Campaign to include the verification and voting
    added a commission interface to interact with commission members.
    commission members & supervisor is liable to verify and approve a voter applicant.
    added voting functionality for the approved voter.
    commission members & supervisor can not vote in the election!

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
+interface CommissionInterface {
+    function numOfMembers() external view returns (uint8);
+
+    function isMember(address _account) external view returns (bool);
+
+    function isSuperVisor(address _user) external view returns (bool);
+}
+
 contract Campaign {
+    address CommissionAddress;
+
     // structure of the users who shall vote
     struct Voter {
         bool verified;
@@ -26,6 +36,7 @@ contract Campaign {
     uint256 public startTime;
 
     constructor(
+        address _commission,
         uint256 _startFrom,
         uint256 _endsIn,
         bytes32[] memory _names
@@ -36,6 +47,7 @@ contract Campaign {
         );
         endTime = _endsIn * 1 seconds;
         startTime = _startFrom * 1 seconds;
+        CommissionAddress = _commission;
         for (uint256 i = 0; i < _names.length && i < 10; i++) {
             candidates.push(Candidate({name: _names[i], voteCount: 0}));
         }
@@ -44,6 +56,46 @@ contract Campaign {
     event VoterVerified(Voter voter);
     event VoterApproved(address indexed voterAddress);
 
+    modifier onlyCommissionMemberOrSuperVisor() {
+        require(
+            isCommissionMember(msg.sender) ||
+                isCommissionSuperVisor(msg.sender),
+            "Only a member or owner can verify!"
+        );
+        _;
+    }
+
+    modifier ownerNotAllowed() {
+        require(!isCommissionSuperVisor(msg.sender), "SuperVisor can not vote");
+        _;
+    }
+
+    modifier membersNotAllowed() {
+        require(!isCommissionMember(msg.sender), "Members can not vote");
+        _;
+    }
+
+    function isCommissionSuperVisor(address _user) public view returns (bool) {
+        CommissionInterface electionCommission = CommissionInterface(
+            CommissionAddress
+        );
+        return electionCommission.isSuperVisor(_user);
+    }
+
+    function isCommissionMember(address _user) public view returns (bool) {
+        CommissionInterface electionCommission = CommissionInterface(
+            CommissionAddress
+        );
+        return electionCommission.isMember(_user);
+    }
+
+    function numOfCommissionMembers() public view returns (uint8) {
+        CommissionInterface electionCommission = CommissionInterface(
+            CommissionAddress
+        );
+        return electionCommission.numOfMembers();
+    }
+
     // user needs to apply for voting before the voting starts
     // some authority might would approve them after that they can vote
     // otherwise there will vote spamming all over the place
@@ -51,6 +103,52 @@ contract Campaign {
         require(!votingEnded(), "Cannot apply after voting has ended");
         require(!votingStarted(), "Cannot apply after voting has started");
         votersList.push(msg.sender);
+    }
+
+    // a voter application is approved only by the majority approval of the commission members
+    function verifyVoter(address _voter)
+        public
+        onlyCommissionMemberOrSuperVisor
+    {
+        require(!votingEnded(), "Cannot verify after voting has ended");
+        require(!isApprovedBy(_voter), "Member already verified the voter");
+        require(
+            !isCommissionMember(_voter),
+            "Cannot verify a member as a voter"
+        );
+        require(
+            !isCommissionSuperVisor(_voter),
+            "Cannot verify the owner as a voter"
+        );
+        Voter storage voter = _voters[_voter];
+        voter.approvalCount++;
+        voter.approvedBy.push(msg.sender);
+        emit VoterVerified(voter);
+        if (voter.approvalCount > numOfCommissionMembers() / 2) {
+            voter.verified = true;
+            emit VoterApproved(_voter);
+        }
+    }
+
+    // function to check if the member has already verified the voter or not
+    function isApprovedBy(address _voter) internal view returns (bool) {
+        Voter memory voter = _voters[_voter];
+        for (uint256 i = 0; i < voter.approvedBy.length; i++) {
+            if (voter.approvedBy[i] == msg.sender) return true;
+        }
+        return false;
+    }
+
+    // function to vote for a candidate
+    function vote(uint8 _to) external ownerNotAllowed membersNotAllowed {
+        require(votingStarted(), "Voting period has not started yet!");
+        require(!votingEnded(), "Voting period has ended!");
+        require(voterIsVerified(), "Voter has no right to vote");
+        require(!voterHasVoted(), "Voter has already casted vote");
+        Voter storage voter = _voters[msg.sender];
+        voter.hasVoted = true;
+        voter.voteTo = _to;
+        candidates[_to].voteCount++;
     }
 
     // function to find the winner of the election
